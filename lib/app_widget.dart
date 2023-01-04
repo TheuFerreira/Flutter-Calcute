cdbfee7552f838d2c6777f1523112eac3cb8ffce
Matheus Ferreira <matheus.44300@gmail.com>
2023-01-03 23:27:25 -0300

    Added font glory to project

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -6,9 +6,10 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Calcute',
-      home: DefaultPage(),
+      theme: ThemeData(fontFamily: 'Glory'),
+      home: const DefaultPage(),
     );
   }
 }
